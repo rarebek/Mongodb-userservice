a3883e233ed572d19117805af6af863bb57aca02
Nodirbek <129874807+rarebek@users.noreply.github.com>
2024-03-19 10:53:04 +0500

    Add files via upload

--- config/config.go
+++ config/config.go
@@ -1,1 +1,45 @@
+package config
 
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	Environment      string // develop, staging, production
+	PostgresHost     string
+	PostgresPort     int
+	PostgresDatabase string
+	PostgresUser     string
+	PostgresPassword string
+	LogLevel         string
+	RPCPort          string
+}
+
+func Load() *Config {
+	c := Config{}
+
+	c.Environment = cast.ToString(getOrReturnDefault("ENVIRONMENT", "develop"))
+
+	c.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "db"))
+	c.PostgresPort = cast.ToInt(getOrReturnDefault("POSTGRES_PORT", 5432))
+	c.PostgresDatabase = cast.ToString(getOrReturnDefault("POSTGRES_DATABASE", "userdb"))
+	c.PostgresUser = cast.ToString(getOrReturnDefault("POSTGRES_USER", "postgres"))
+	c.PostgresPassword = cast.ToString(getOrReturnDefault("POSTGRES_PASSWORD", "123"))
+
+	c.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
+
+	c.RPCPort = cast.ToString(getOrReturnDefault("RPC_PORT", ":5000"))
+
+	return &c
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+	if exists {
+		return defaultValue
+	}
+
+	return defaultValue
+}
